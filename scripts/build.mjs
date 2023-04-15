6bdec700c6de47e948caeeb68b63cc917c261248
xiaoyu <7547189+xytoki@users.noreply.github.com>
2023-04-15 20:50:30 +0800

    🐎 ci: build dist pkg on linux

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -3,14 +3,26 @@ if (os.platform() !== 'linux') {
     echo`>> This script is only for Linux.`;
     exit(1);
 }
-let suffix = ''
+await cd('tailscale');
+await fs.ensureDir('../dist');
 if (process.argv.includes('--windows')) {
     // cross for windows amd64
     process.env.GOOS = 'windows';
     process.env.GOARCH = 'amd64';
-    suffix = '.exe';
+    await $`./build_dist.sh -v -o ../dist/tailscale.exe tailscale.com/cmd/tailscale`;
+    await $`./build_dist.sh -v -o ../dist/tailscaled.exe tailscale.com/cmd/tailscaled`;
+} else {
+    $.env = {
+        PATH: process.env.PATH,
+        HOME: process.env.HOME,
+    };
+    await $`./tool/go run ./cmd/dist build linux/amd64/*`;
+    cd('..');
+    const distpkgs = await glob('tailscale/dist/*.*');
+    for (const pkg of distpkgs) {
+        const fpath = path.resolve(pkg);
+        const fname = path.basename(fpath).split('_');
+        fname[1] = 'patched';
+        await fs.move(fpath, `dist/${fname.join('-')}`);
+    }
 }
-await cd('tailscale');
-await fs.ensureDir('../dist');
-await $`./build_dist.sh -v -o ../dist/tailscale${suffix} tailscale.com/cmd/tailscale`;
-await $`./build_dist.sh -v -o ../dist/tailscaled${suffix} tailscale.com/cmd/tailscaled`;
