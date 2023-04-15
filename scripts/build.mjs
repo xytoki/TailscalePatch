6eff13c8e717013311137c5963a83eef818e6585
xiaoyu <7547189+xytoki@users.noreply.github.com>
2023-04-15 12:26:51 +0800

    🐎 ci: add ci matrix

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -3,10 +3,14 @@ if (os.platform() !== 'linux') {
     echo`>> This script is only for Linux.`;
     exit(1);
 }
-// cross for windows amd64
-process.env.GOOS = 'windows';
-process.env.GOARCH = 'amd64';
+let suffix = ''
+if (process.argv.includes('--windows')) {
+    // cross for windows amd64
+    process.env.GOOS = 'windows';
+    process.env.GOARCH = 'amd64';
+    suffix = '.exe';
+}
 await cd('tailscale');
 await fs.ensureDir('../dist');
-await $`./build_dist.sh -v -o ../dist/tailscale.exe tailscale.com/cmd/tailscale`;
-await $`./build_dist.sh -v -o ../dist/tailscaled.exe tailscale.com/cmd/tailscaled`;
+await $`./build_dist.sh -v -o ../dist/tailscale${suffix} tailscale.com/cmd/tailscale`;
+await $`./build_dist.sh -v -o ../dist/tailscaled${suffix} tailscale.com/cmd/tailscaled`;
